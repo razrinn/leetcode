737620ecbf5d448c3c50fa5e04eddbee496fe38d
Ray Azrin Karim <ray.karim@stockbit.com>
2024-11-12 23:14:04 +0700

    leetcode 217

---
+++ 217_contains_duplicate.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        dic = {}
+        for i in nums:
+            if i not in dic:
+                dic[i] = True
+            else:
+                return True
+        return False

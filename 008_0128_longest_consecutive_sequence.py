c1fdafc850241ea40bd8df24fd325ce1bf90cad7
Ray Azrin Karim <ray.karim@stockbit.com>
2025-02-23 21:28:32 +0700

    fix TLE

--- 008_0128_longest_consecutive_sequence.py
+++ 008_0128_longest_consecutive_sequence.py
@@ -1,6 +1,6 @@
 from typing import List
 
-# time limit exceptions
+# fix time limit exception by changing starts from list to hashmap
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
         if len(nums) == 0:
@@ -10,15 +10,15 @@ class Solution:
         for num in nums:
             hmap[num] = True
 
-        starts = []
+        starts = {}
 
         for num in nums:
             if num - 1 not in hmap:
-                starts.append(num)
+                starts[num] = True
 
         tmp = 1
         result = 1
-        for num in starts:
+        for num in starts.keys():
             i = num + 1
             while i in hmap:
                 tmp += 1

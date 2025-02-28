45785689eed40e4e587b8b32f2690c52248d0f25
Ray Azrin Karim <ray.karim@stockbit.com>
2025-02-28 10:23:06 +0700

    binary search

---
+++ 019_0704_binary_search.py
@@ -0,0 +1,17 @@
+from typing import List
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        res = -1
+        l = 0
+        r = len(nums) - 1
+
+        while l <= r:
+            m = (l + r) // 2
+            if nums[m] == target:
+                return m
+            if nums[m] < target:
+                l = m + 1
+            else:
+                r = m - 1
+        return -1

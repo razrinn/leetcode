f5b02dca9ed3774d119a84da6b2400a058456b91
Ray Azrin Karim <ray.karim@stockbit.com>
2025-03-03 20:00:10 +0700

    group anagrams. 2nd

--- 004_0049_group_anagrams.py
+++ 004_0049_group_anagrams.py
@@ -8,3 +8,13 @@ class Solution:
             hmap[s_word] = hmap.get(s_word, []) + [word]
 
         return list(hmap.values())
+
+    # second run, use defaultdict
+    def groupAnagrams2(self, strs: List[str]) -> List[List[str]]:
+        hmap = defaultdict(list)
+
+        for word in strs:
+            s_word = "".join(sorted(word))
+            hmap[s_word].append(word)
+
+        return list(hmap.values())

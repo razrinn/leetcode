2f81b7daee25024d047e9900b01506ed5c08e8d5
Ray Azrin Karim <ray.karim@stockbit.com>
2025-03-03 08:41:24 +0700

    course schedule

---
+++ 048_0207_course_schedule.py
@@ -0,0 +1,37 @@
+from collections import defaultdict
+from typing import List
+
+# assisted by chatgpt, need revisit
+
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        graph = defaultdict(list)
+
+        for [course, prereq] in prerequisites:
+            graph[course].append(prereq)
+
+        visiting = set()
+        visited = set()
+
+        def dfs(course: int):
+            if course in visiting:
+                return False
+
+            if course in visited:
+                return True
+
+            visiting.add(course)
+            for neighbor in graph[course]:
+                if not dfs(neighbor):
+                    return False
+
+            visiting.remove(course)
+            visited.add(course)
+
+            return True
+
+        for course in range(numCourses):
+            if not dfs(course):
+                return False
+
+        return True

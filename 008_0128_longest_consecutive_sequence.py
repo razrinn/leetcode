6302b0d282ab628c1408a5da86e393f916a0619c
Ray Azrin Karim <ray.karim@stockbit.com>
2025-03-03 11:43:03 +0700

    128 and 146 second run

--- 008_0128_longest_consecutive_sequence.py
+++ 008_0128_longest_consecutive_sequence.py
@@ -27,3 +27,21 @@ class Solution:
             tmp = 1
 
         return result
+
+    # second run, more efficient
+    def longestConsecutive2(self, nums: List[int]) -> int:
+        firsts = set()
+        hmap = set(nums)
+
+        for num in hmap:
+            if num - 1 not in hmap:
+                firsts.add(num)
+
+        res = 0
+        for num in firsts:
+            tmp = 1
+            while num + tmp in hmap:
+                tmp += 1
+            res = max(res, tmp)
+
+        return res
--- 027_0146_lru_cache.py
+++ 027_0146_lru_cache.py
@@ -47,3 +47,46 @@ class ListNode:
 # obj = LRUCache(capacity)
 # param_1 = obj.get(key)
 # obj.put(key,value)
+
+# second run
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.cache = {}
+        self.cap = capacity
+        self.start, self.end = ListNode(0,0), ListNode(0,0)
+        self.start.next, self.end.prev = self.end, self.start
+
+    def remove(self, node):
+        prev, nxt = node.prev, node.next
+        prev.next, nxt.prev = nxt, prev
+
+        del self.cache[node.key]
+
+    def add(self, key: int, val: int) -> ListNode:
+        new_node = ListNode(key, val)
+        prev = self.end.prev
+        prev.next, self.end.prev = new_node, new_node
+        new_node.next, new_node.prev = self.end, prev
+
+        self.cache[key] = new_node
+
+
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1
+
+        val = self.cache[key].val
+        self.remove(self.cache[key])
+        self.add(key, val)
+
+        return val
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self.remove(self.cache[key])
+        self.add(key, value)
+        if len(self.cache) > self.cap:
+            self.remove(self.start.next)
+
+
